d1a5655f3349d1a6a35dc8732da9a56f7af764ab
Fabio Scarpa <fscarpa20#gmail.com>
2025-02-04 09:08:52 +0100

    completed exercise

--- main.py
+++ main.py
@@ -14,14 +14,31 @@ def main():
     def char_count(file):
         
         counter_dict = {}
+        letters_list = []
+
         for char in file:
             char = char.lower()
             if char not in counter_dict:
                 counter_dict[char] = 1
             else:
                 counter_dict[char] += 1
-               
-        return counter_dict
+            
+        #counter_dict = dict(list(sorted(counter_dict.items()))[32:])
+        counter_dict = list(counter_dict.items())
+        #print([list(counter_dict[0])])
+        for i in range(len(counter_dict)):
+            character = list(counter_dict[i])
+            letters_list.append(character)
+            letters_list.sort()
+        
+        
+        #print(letters_list[32:])
+        for data in letters_list[32:]:
+            print(f"The '{data[0]}' character was found {data[1]} times")
+    
+
+        return "---End report---"
+        
     
     print(char_count(file_contents))
 
